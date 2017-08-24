1ce80b480ea45a759abe64f082c376959fee624d
Aonghus O Nia <aonghusonia@eircom.net>
2017-08-24 20:36:30 +0000

    changed question phrasing

--- reverse.js
+++ reverse.js
@@ -7,7 +7,7 @@ const reverse = (goal, numbers = shuffle(goal), step = 0) =>
   JSON.stringify(numbers) === JSON.stringify(goal)
   ? console.log(`Done! That took you ${step} steps!`)
   : reverse(goal,
-    flip(numbers, rl.question(`${numbers.join(' ')}\nHow many numbers to swap? `)),
+    flip(numbers, rl.question(`${numbers.join(' ')}\nReverse how many? `)),
     step + 1);
 
 reverse([1, 2, 3, 4, 5, 6, 7, 8, 9]);
