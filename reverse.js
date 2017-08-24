4bf48a4e96333d7413926301d5cc1c3abf4799ff
Aonghus O Nia <aonghusonia@eircom.net>
2017-08-24 19:35:37 +0000

    first commit

---
+++ reverse.js
@@ -0,0 +1,14 @@
+const rl = require('readline-sync');
+const shuffle = require('lodash.shuffle');
+
+const flip = (ar, n) => ar.slice(0, n).reverse().concat(ar.slice(n, ar.length));
+
+const reverse = (goal, numbers = shuffle(goal), step = 0) =>
+  JSON.stringify(numbers) === JSON.stringify(goal)
+  ? console.log(`Done! That took you ${step} steps!`)
+  : reverse(goal,
+    flip(numbers, rl.question(`${numbers.join(' ')}\nHow many numbers to swap? `)),
+    step + 1);
+
+reverse([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+
