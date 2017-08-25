b33bceaec4a711bc15b14ab706f99b07f898aed4
Aonghus O Nia <aonghusonia@eircom.net>
2017-08-25 19:59:51 +0000

    use readline

--- reverse.js
+++ reverse.js
@@ -1,10 +1,13 @@
 "use strict";
 
-const rl = require('readline-sync');
+const rl = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
 const swap = (ar, i, j) => i === j
-    ? ar
-    : [...ar.slice(0, i), ar[j], ...ar.slice(i + 1, j), ar[i], ...ar.slice(j + 1)];
+  ? ar
+  : [...ar.slice(0, i), ar[j], ...ar.slice(i + 1, j), ar[i], ...ar.slice(j + 1)];
 
 const randInt = max => Math.floor(Math.random() * (max + 1));
 
@@ -12,12 +15,18 @@ const shuffle = ar => ar.reduceRight((pv, _, i) => swap(pv, randInt(i), i), ar);
 
 const flip = (ar, n) => ar.slice(0, n).reverse().concat(ar.slice(n, ar.length));
 
-const reverse = (goal, numbers = shuffle(goal), step = 0) =>
-  JSON.stringify(numbers) === JSON.stringify(goal)
-  ? console.log(`Done! That took you ${step} steps!`)
-  : reverse(goal,
-    flip(numbers, rl.question(`${numbers.join(' ')}\nReverse how many? `)),
-    step + 1);
+const eq = (ar1, ar2) => ar1.every((v, i) => v === ar2[i]);
+
+const reverse = (goal, numbers = shuffle(goal), step = 0) => {
+  if (eq(numbers, goal)) {
+    console.log(`Done! That took you ${step} steps!`);
+    rl.close();
+  }
+  else {
+    rl.question(`${numbers.join(' ')}\nReverse how many? `,
+      n => reverse(goal, flip(numbers, n), step + 1)
+    );
+  }
+};
 
 reverse([1, 2, 3, 4, 5, 6, 7, 8, 9]);
-
