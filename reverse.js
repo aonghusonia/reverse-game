0383009b216852b5474ac4832daf5ca1692003e2
Aonghus O Nia <aonghusonia@eircom.net>
2017-08-25 16:16:18 +0000

    added shuffle

--- reverse.js
+++ reverse.js
@@ -1,5 +1,14 @@
+"use strict";
+
 const rl = require('readline-sync');
-const shuffle = require('lodash.shuffle');
+
+const swap = (ar, i, j) => i === j
+    ? ar
+    : [...ar.slice(0, i), ar[j], ...ar.slice(i + 1, j), ar[i], ...ar.slice(j + 1)];
+
+const randInt = max => Math.floor(Math.random() * (max + 1));
+
+const shuffle = ar => ar.reduceRight((pv, _, i) => swap(pv, randInt(i), i), ar);
 
 const flip = (ar, n) => ar.slice(0, n).reverse().concat(ar.slice(n, ar.length));
 
